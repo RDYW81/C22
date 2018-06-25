3e80bf04eec023daf3fc63d0b92681b9f0085911
RDYW81 <r.daidone@t-online.de>
2018-06-26 00:07:29 +0200

    Formatierung + Kommentar hinzugefuegt

--- main.c
+++ main.c
@@ -8,19 +8,25 @@ int main (){
     float skonto;
     float euro;
 
-
+        /*
+         * Hier findet die Preisberechnung statt.
+         * Als Eingabe erwartet wird ein Nettopreis (als int oder float)
+         * In erster Folge werden 20% MwSt hinzugefuegt.
+         * In zweiter Folge werden 2& Skonto abgezogen.
+         * Ausgabe: Rechnungsbetrag.
+         */
         printf("Nettopreis eingeben: ");
         scanf("%f", &eingabebetrag);
         zwischensumme = eingabebetrag / mwst;
-        printf("Nettopreis           Euro %4f\n", eingabebetrag);
-        euro = eingabebetrag + zwischensumme;;
-        printf("+ 20%% MwSt           Euro %4f\n", zwischensumme);
-        printf("===================================\n");
-        printf("Bruttopreis          Euro %4f\n", euro);
+        printf("Nettopreis           Euro %4.2f\n", eingabebetrag);
+        euro = eingabebetrag + zwischensumme;
+        printf("+ 20%% MwSt           Euro %4.2f\n", zwischensumme);
+        printf("==================================\n");
+        printf("Bruttopreis          Euro %4.2f\n", euro);
         skonto = euro / skon;
-        printf("- 2%% Skonto          Euro %4f\n", skonto);
-        printf("===================================\n");
+        printf("- 2%% Skonto          Euro %4.2f\n", skonto);
+        printf("==================================\n");
         euro = euro - skonto;
-        printf("Rechnungsbetrag      Euro %4f\n", euro);
+        printf("Rechnungsbetrag      Euro %4.2f\n", euro);
 
 }
